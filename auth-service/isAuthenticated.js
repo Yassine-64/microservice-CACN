5b13944d67ed2b223f38fb41786556a53e0b89cc
Yassine.io <yassinemazhare@gmail.com>
2025-04-06 14:37:05 +0100

    Add authentication middleware to Auth Service

--- auth-service/isAuthenticated.js
+++ auth-service/isAuthenticated.js
@@ -1,6 +1,11 @@
+/**
+ * Authentication middleware
+ * Verifies JWT token from request headers
+ */
 const jwt = require("jsonwebtoken");
 
 module.exports = async function isAuthenticated(req, res, next) {
+  // Extract token from Authorization header
   const token = req.headers["authorization"]?.split(" ")[1];
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
